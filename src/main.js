145dc78a69a96e41082ff544851bbe8fa4dc4b6c
Deniz <48476031+DenizzOzder@users.noreply.github.com>
2025-06-16 11:25:30 +0300

    Update main.js
    Import özellikleri koşula bağlandı

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import './js/theme';
 import './js/fetchApi'
-import './js/catalog-hero'
-import './js/catalog-list'
 import './js/footer'
+const path = window.location.pathname;
+console.log(path)
+if (path.endsWith('catalog.html')) {
+  import('./js/catalog-list');
+  import('./js/catalog-hero');
+}
