b33bdb522103643a2d3bc322e5b9d318641428b8
Deniz <48476031+DenizzOzder@users.noreply.github.com>
2025-06-16 21:51:04 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import './js/theme';
 import './js/fetchApi'
 import './js/footer'
-const path = window.location.pathname;
-console.log(path)
-if (path.endsWith('catalog.html')) {
-  import('./js/catalog-list');
-  import('./js/catalog-hero');
-}
+
